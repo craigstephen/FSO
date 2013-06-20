6a3ccf89f623d5eb134b4bf7ff436aae55223b43
craigstephen <craig@craigstephen.co.uk>
2013-06-20 13:13:20 +0100

    Upload

---
+++ assets/js/map.js
@@ -0,0 +1,38 @@
+function initialize() {
+
+  // Create an array of styles.
+  var styles = [
+    {
+      stylers: [
+        { "saturation": -100 },
+        { "weight": 0.5 },
+        { "lightness": 14 },
+        { "gamma": 1.04 }
+      ]
+    }
+  ];
+
+
+  // Create a new StyledMapType object, passing it the array of styles,
+  // as well as the name to be displayed on the map type control.
+  var styledMap = new google.maps.StyledMapType(styles,
+    {name: "Styled Map"});
+
+  // Create a map object, and include the MapTypeId to add
+  // to the map type control.
+  var mapOptions = {
+    zoom: 11,
+    center: new google.maps.LatLng(55.6468, 37.581),
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+    }
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
+
+  //Associate the styled map with the MapTypeId and set it to display.
+  map.mapTypes.set('map_style', styledMap);
+  map.setMapTypeId('map_style');
+}
+
+initialize();
